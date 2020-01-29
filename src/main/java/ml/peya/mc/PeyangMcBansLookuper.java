319821c11648d816ee649467b01674a66bd06af7
peyang-Celeron <mikasa.chibimasu@gmail.com>
2020-01-29 17:10:18 +0900

    add lang file

--- src/main/java/ml/peya/mc/PeyangMcBansLookuper.java
+++ src/main/java/ml/peya/mc/PeyangMcBansLookuper.java
@@ -71,7 +71,6 @@ public class PeyangMcBansLookuper
     @EventHandler
     public void init(FMLInitializationEvent e)
     {
-        System.out.println("Hello, World!");
         MinecraftForge.EVENT_BUS.register(new Events());
         lookupKeys = new KeyBinding("Check the player's lookup at", 38, "PeyangMcBansLookup");
         ClientRegistry.registerKeyBinding(lookupKeys);
